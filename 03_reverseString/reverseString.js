6f084a4b63e9569146b22f0dc1c0b1ff2168d88d
anuragjodha123 <anuragjodha123@gmail.com>
2025-02-13 02:47:49 +0000

    completed third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  let reverseTextArr = [];
 
+  // reverses text
+  for (let i = 1; i <= text.length; i++){
+    let reverseOrder = text.length - i;
+    reverseTextArr.push(text[reverseOrder]);
+  }
+
+  // converts to string
+  let reverseText = reverseTextArr.join('');
+
+  return reverseText;
 };
 
 // Do not edit below this line
