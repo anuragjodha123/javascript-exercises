66f0ab5579c402488ec1bcc91ba55cdc2b58b91f
anuragjodha123 <anuragjodha123@gmail.com>
2025-02-14 02:16:32 +0000

    completed fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+  // use the sum of a sequence formula
+  // S = n/2 (1st term + last term)
+  if (typeof(firstInt) == "number" && typeof(lastInt) == "number" 
+      && parseInt(firstInt) === firstInt && parseInt(lastInt) === lastInt
+      && firstInt*lastInt >= 0) 
+  {
+    let numberOfInts = (lastInt > firstInt) ? (lastInt - firstInt) + 1 : (firstInt - lastInt) + 1; 
+    let sumOfInts = (numberOfInts/2)*(firstInt + lastInt);
+    return sumOfInts;
+  } else {
+    return "ERROR";
+  }
 
 };
 
