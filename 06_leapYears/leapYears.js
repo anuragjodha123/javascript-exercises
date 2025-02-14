0c6dc74bd0e1f88c6d37951b7104a02ada606559
anuragjodha123 <anuragjodha123@gmail.com>
2025-02-14 02:49:16 +0000

    completed the leap year exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+// check if divisible by 100
+// if yes -> check divisible if divisible by 400
+// if also yes -> leap year "wow"
+// else check if divisible by 4
+// if yes ->  leap year "wow"
+// else not leap year
+
+if (year % 100 === 0) {
+  if (year % 400 === 0) return true;
+  else return false;
+} else if(year % 4 === 0) {
+  return true;
+} else {
+  return false;
+}
 
 };
 
